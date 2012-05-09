d7922f2378496764d6172012f74481bd28d8a1cb
Ehsan Nourbakhsh <ehsaan@gmail.com>
2012-05-09 14:43:19 -0500

    changed the username/password so now it is read from config. standard, I know, and should have done from day one.

--- config.php
+++ config.php
@@ -2,4 +2,7 @@
 $erman_address="http://dslab.utdallas.edu/~ehsann/sharedride/";
 $phpmailer_path="lib/PHPMailer_5.2.0";
 $email_enabled=1;
+$db_username="";
+$db_password="";
+$db_host="dslabcomp";
 ?>
