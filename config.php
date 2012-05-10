4a299a07f6f54b510da503ab7bc1978b3221231c
Ehsan Nourbakhsh <ehsaan@gmail.com>
2012-05-09 22:44:58 -0500

    change to config for DB username/password

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 $erman_address="http://dslab.utdallas.edu/~ehsann/sharedride/";
 $phpmailer_path="lib/PHPMailer_5.2.0";
 $email_enabled=1;
-$db_username="ehsansr";
-$db_password="qrdKsbWY4Psj6fXK";
-$db_host="dslabcomp";
-$db_database="ehsansr";
+$db_username="";
+$db_password="";
+$db_host="";
+$db_database="";
 ?>
